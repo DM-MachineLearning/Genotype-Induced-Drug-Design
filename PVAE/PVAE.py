220b83284169c8a3e9f9ad058a271506b5b3519c
Rudra-G616 <rudrag2377@gmail.com>
2025-12-07 18:01:10 +0530

    Fixing Import Paths

--- PVAE/PVAE.py
+++ PVAE/PVAE.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import copy
-from utils import celoss, SelfAttention
+from .utils import celoss, SelfAttention
 
 
 class pVAE(nn.Module):
