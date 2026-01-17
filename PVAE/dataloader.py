06734c63f363886a95f08c39a54410c6143c1360
Your Name <you@example.com>
2026-01-17 16:15:18 +0530

    Adding Dataloader for Supervised Learning

--- PVAE/dataloader.py
+++ PVAE/dataloader.py
@@ -15,6 +15,23 @@ class MultiOmicsDataset(Dataset):
     def __getitem__(self, idx):
         return self.dna[idx], self.gene[idx]
     
+class MultiOmicsDatasetSupervised(Dataset):
+    def __init__(self, dna_data, gene_data, labels):
+        """
+        dna_data: tensor (N, input_dim_dna)
+        gene_data: tensor (N, input_dim_gene)
+        labels: tensor (N,) or (N, num_classes)
+        """
+        self.dna = dna_data
+        self.gene = gene_data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.dna)
+
+    def __getitem__(self, idx):
+        return self.dna[idx], self.gene[idx], self.labels[idx]
+    
     
     
 def return_dataloaders(X_dna_meth, X_gene_exp, split_fractions : tuple = (0.8, 0.1)):
@@ -29,9 +46,27 @@ def return_dataloaders(X_dna_meth, X_gene_exp, split_fractions : tuple = (0.8, 0
 
     train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
 
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    val_loader   = DataLoader(val_dataset,   batch_size=64, shuffle=False)
-    test_loader  = DataLoader(test_dataset,  batch_size=64, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+    val_loader   = DataLoader(val_dataset,   batch_size=256, shuffle=False)
+    test_loader  = DataLoader(test_dataset,  batch_size=256, shuffle=False)
+
+    return train_loader, val_loader, test_loader
+
+def return_dataloaders_supervised(X_dna_meth, X_gene_exp, labels, split_fractions : tuple = (0.8, 0.1)):
+
+    """ split_fractions (tuple) : fractional sizes of training and validation set """
+
+    dataset = MultiOmicsDatasetSupervised(X_dna_meth, X_gene_exp, labels)
+
+    train_size = int(split_fractions[0]*len(dataset))
+    val_size = int(split_fractions[1]*len(dataset))
+    test_size = int(len(dataset)) - (train_size + val_size)
+
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+    val_loader   = DataLoader(val_dataset,   batch_size=256, shuffle=False)
+    test_loader  = DataLoader(test_dataset,  batch_size=256, shuffle=False)
 
     return train_loader, val_loader, test_loader
 
